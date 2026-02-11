f9b61b69f763361701ee14c646fd99e34e4e4ddc
NickMillburn <n340028@gmail.com>
2026-02-11 12:21:50 -0600

    Temperature sensing working for device is working now.

--- Lab1/Lab 1/src/main.cpp
+++ Lab1/Lab 1/src/main.cpp
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 // Based on "Simple webserver example from https://docs.arduino.cc/tutorials/uno-r4-wifi/wifi-examples/"
 
 #include <Arduino.h>
@@ -148,3 +149,40 @@ void printWifiStatus() {
   Serial.print(rssi);
   Serial.println(" dBm");
 }
+=======
+#include <OneWire.h>
+#include <DallasTemperature.h>
+
+constexpr uint8_t PIN_A = 2;   // Sensor A DQ
+constexpr uint8_t PIN_B = 3;   // Sensor B DQ
+
+OneWire oneWireA(PIN_A);
+OneWire oneWireB(PIN_B);
+
+DallasTemperature sensorA(&oneWireA);
+DallasTemperature sensorB(&oneWireB);
+
+void setup() {
+  Serial.begin(115200);
+
+  
+  pinMode(PIN_A, INPUT_PULLUP); 
+  pinMode(PIN_B, INPUT_PULLUP);
+
+  sensorA.begin();
+  sensorB.begin();
+}
+
+void loop() {
+  sensorA.requestTemperatures();
+  sensorB.requestTemperatures();
+
+  float tA = sensorA.getTempCByIndex(0);
+  float tB = sensorB.getTempCByIndex(0);
+
+  Serial.print("A: "); Serial.print(tA); Serial.print(" C   ");
+  Serial.print("B: "); Serial.print(tB); Serial.println(" C");
+
+  delay(1000);
+}
+>>>>>>> 42d634d (Temperature sensing working for device is working now.)
---
+++ Lab1/Lab 1/src/sensors.cpp
@@ -0,0 +1,11 @@
+/*int TempInput1 = 2;
+int TempInput2 = 3;
+
+void loop() 
+{
+
+  int sensorVal1 = digitalRead(TempInput1);
+  int sensorVal2 = digitalRead(TempInput2); // Reads the value from the specified input pin
+  // Use sensorVal in your logic
+}
+  */
