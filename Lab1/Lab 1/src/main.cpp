2648992c40b64d5fa3d169443b9e0a26faf13711
in-sink <1bashkimuseni1@gmail.com>
2026-02-18 14:03:06 -0600

    moved server logic

--- Lab1/Lab 1/src/main.cpp
+++ Lab1/Lab 1/src/main.cpp
@@ -6,6 +6,7 @@
 #include "display.h"
 #include "sensors.h"
 #include <RTC.h>
+#include "TempServer.cpp"
 
 //getting wifi credientials from separate file for security purposes
 char ssid[] = PRIVATE_SSID; // your network SSID (name)
@@ -13,7 +14,8 @@ char password[] = PRIVATE_PASSWORD; // your network password (use for WPA, or us
 
 int led = LED_BUILTIN;
 int status = WL_IDLE_STATUS;
-WiFiServer server(80);
+
+TempServer server; // Create an instance of the TempServer class to manage WiFi and server functions
 
 // put function declarations here:
 // function to print WiFi status to serial monitor, including the IP address of the board, network SSID, and signal strength:
@@ -34,7 +36,7 @@ void setup() {
   if (fv < WIFI_FIRMWARE_LATEST_VERSION) {
     Serial.println("Please upgrade the firmware");
   } 
-  
+
   //attempt to connect to WiFi network:
 
   while (status != WL_CONNECTED) {
@@ -47,17 +49,17 @@ void setup() {
     delay(10000);
   }
 
-  server.begin();
   printWifiStatus();
 
   display_init();
   sensors_init();
+  server = TempServer(80); // Initialize the TempServer instance to listen on port 80
 
   Serial.println("Boot complete.");
 }
 
 void loop() {
-  WiFiClient client = server.available();   // listen for incoming clients
+  WiFiClient client = server.getClient();   // listen for incoming clients
   
   if (client) {                             // if you get a client,
     Serial.println("new client");           // print a message out the serial port
@@ -75,6 +77,9 @@ void loop() {
             // and a content-type so the client knows what's coming, then a blank line:
             client.println("HTTP/1.1 200 OK");
             client.println("Content-type:text/html");
+            client.println("Connection: close");
+            client.println("Refresh: 1"); //refresh page automatically every 1 sec
+
             client.println();
 
             // the content of the HTTP response follows the header:
